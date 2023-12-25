07dd16d3ed59dcaeb945d0cc1aafad46e724a78c
learner-susu <gyawalisusu61@gmail.com>
2023-12-25 19:58:12 +0545

    while_loop

---
+++ array_more_programs/do-while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   
+   int n,i = 0;
+    printf("Enter any number");
+    scanf("%d",&n);
+   do{
+       printf("Sushmi\n");
+        i++;
+   }while(i<n);
+   printf("out of loop");
+   
+    return 0;
+}
