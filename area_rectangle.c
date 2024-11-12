0291623ebe8ef10e236ba3c58c8861a03a9a264a
learner-susu <91686558+learner-susu@users.noreply.github.com>
2024-11-12 18:26:58 +0545

    Add files via upload

---
+++ area_rectangle.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+ 
+   int main(){
+    int l,b,a;
+
+    printf( "Enter leanth ");
+    scanf("%d",&l);
+    printf("Enter breath ");
+    scanf("%d",&b);
+    a = l * b;
+    printf(" Area is %d\n",a);
+
+    return 0;
+   }
